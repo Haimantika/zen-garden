bb3b1c21d4f1643103bbddf8409818294eabac53
Shubham Sah <shubhamsah227644@gmail.com>
2024-07-09 23:24:39 +0530

    solvign timeout error

--- api/app.js
+++ api/app.js
@@ -15,17 +15,21 @@ app.post('/api/care-routine', async (req, res) => {
     const plantName = req.body.plantName;
     console.log("Sending request to OpenAI with plant name:", plantName);
     try {
+        console.time("Open Api Call")
         const apiRequest = {
             model: "gpt-3.5-turbo",
             messages: [{ role: "user", content: `I have ${plantName}. Can you share a full care routine for this plant in 100 words?` }]
         };
         console.log("Request body to OpenAI:", JSON.stringify(apiRequest));
+
         const response = await axios.post(OPENAI_API_URL, apiRequest, {
             headers: {
                 'Authorization': `Bearer ${OPENAI_API_KEY}`,
                 'Content-Type': 'application/json'
-            }
+            }, 
+            timeout: 25000 
         });
+        console.timeEnd("OpenAPI Call")
         console.log("Received response from OpenAI:", JSON.stringify(response.data));
         
         if (response.data.choices && response.data.choices.length > 0) {
